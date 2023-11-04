5994c61ba8e3c7f6829cc0bd0c6385b8afcf056b
suhelk7057 <44229028+suhelk7057@users.noreply.github.com>
2023-11-04 11:38:57 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,25 @@
+// console.log(2 > 1);
+// console.log( 2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+console.log("2" > 1);
+console.log("02" > 1);
+
+// console.log( null > 0);
+// console.log( null == 0);
+// console.log( null >= 0);
+
+// Avoid this comparision
+
+// console.log(undefined ==0);
+// console.log(undefined >=0);
+// console.log(undefined <=0);
+// console.log(undefined >0);
+// console.log(undefined <0);
+
+// === Use this comparision
+
+console.log("2" ===2);
