c8bc57a2375706909459170944bdb06a4b7f16a7
suhelk7057 <44229028+suhelk7057@users.noreply.github.com>
2023-11-04 11:27:13 +0000

    Conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -25,5 +25,76 @@ let someNumber = 33
 
 let stringNumber = String(someNumber)
 
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// *********** Operations ***********
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2**3);
+// console.log(2 % 3);
+
+let str1 = "hello"
+let str2  = " suhel"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log( 1 + "2");
+// console.log( "1" + 2 + 2);
+// console.log( 1 + 2 + "2" );
+
+// console.log( ( 3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// link to study
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
+
+
+// Prefix increment
+// let x = 3;
+// const y = ++x;
+// // x is 4; y is 4
+
+// let x2 = 3n;
+// const y2 = ++x2;
+// x2 is 4n; y2 is 4n
+
+// Postfix increment
+
+// let x = 3;
+// const y = x++;
+// // x is 4; y is 3
+
+// let x2 = 3n;
+// const y2 = x2++;
+// x2 is 4n; y2 is 3n
