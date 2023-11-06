b84f227df4f2a2af952444ae4ca3a6f0e7a73f0c
suhelk7057 <44229028+suhelk7057@users.noreply.github.com>
2023-11-06 06:19:34 +0000

    dates in js

---
+++ 01_basics/07_datesin_js.js
@@ -0,0 +1,34 @@
+// Dates
+
+let myDate = new Date()
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toISOString());
+// console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2023, 0, 23)
+// console.log(myCreatedDate.toDateString());
+// let myCreatedDate = new Date(2023, 0, 23, 5, 3)
+// console.log(myCreatedDate.toLocaleString());
+// let myCreatedDate = new Date("2023-01-14")
+let myCreatedDate = new Date("01-14-2023")
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// c
+
+let newDate = new Date()
+// console.log(newDate);
+// console.log(newDate.getMonth()+1);
+// console.log(newDate.getDay());
+// console.log(newDate.getFullYear());
+
+// `${newDate.getDay()} and the time`
+
+newDate.toLocaleString('default', {
+    weekday: "long",
+})
